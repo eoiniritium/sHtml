170af9324e268a7acae9b264240deb0f3c7010ef
Eoin <mazecraze06@gmail.com>
2021-12-16 01:07:37 +0000

    Init

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    let html = t.render('./src/index.html', {name: 'Eoin', surname: 'Mag Ualghairg'});
+    let html = t.render('./src/index.html', {name: 'Your name', surname: 'Your surname'});
     res.send(html);
 });
 
--- src/javascript.js
+++ src/javascript.js
@@ -1,6 +1,6 @@
 function addtext() {
     let p_tag = document.createElement("P");
-    let t = document.createTextNode("This is a paragraph.");
+    let t = document.createTextNode("Text added to div!");
     p_tag.appendChild(t);
     document.getElementById("div").appendChild(p_tag);
 }
